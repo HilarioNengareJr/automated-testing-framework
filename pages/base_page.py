b9f77da908c0815715913c8096649fcc796ab596
Hilario Nengare <hnengare@gmail.com>
2023-10-23 15:24:58 +0300

    Added base page.

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        
+    def open(self, url):
+        self.driver.get(url)
+    
+    def wait_for_element_to_be_visible(self, locator, timeout=10):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+        except TimeoutException:
+            raise AssertionError(f"Element with locator {locator} not visible within {timeout} seconds.")
